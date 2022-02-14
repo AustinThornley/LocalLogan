c5dba3c1d0ccc2ebb1c3d9aeeefe636c04c8d847
Austin Thornley <austinjt1012@gmail.com>
2022-02-13 20:10:28 -0700

    Initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+	<h1 class="text-center">Hello world</h1>
+	<p class="text-center font-bold">{{ msg }} - and bold!</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
